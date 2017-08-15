ec7e2d4930bfd40be3bedb3d9481470810b202b6
jiahaodong <jia.haodong@21vianet.com>
2017-08-15 08:01:16 +0800

    加入layui

---
+++ src/main/java/evdc/vianet/auth/entity/User.java
@@ -0,0 +1,79 @@
+package evdc.vianet.auth.entity;
+
+public class User {
+	public static final String TABLE_NANE = "tier_user";
+	private int id;
+	private String name;
+	private String role;
+	private int teamId;
+	private String phone;
+	private String email;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public int getTeamId() {
+		return teamId;
+	}
+
+	public void setTeamId(int teamId) {
+		this.teamId = teamId;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static String getTableNane() {
+		return TABLE_NANE;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", role=" + role + ", teamId=" + teamId + ", phone=" + phone
+				+ ", email=" + email + ", password=" + password + "]";
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +7,15 @@
 <title>EvDC</title>
 </head>
 <body>
-工单系统
+	工单系统是
+	<!-- 你的HTML代码 -->
+	<script src="<%=request.getContextPath()%>/layui/layui.js"></script>
+	<script>
+		//一般直接写在一个js文件中
+		layui.use([ 'layer', 'form' ], function() {
+			var layer = layui.layer, form = layui.form();
+			layer.msg('Hello jhd World');
+		});
+	</script>
 </body>
 </html>
